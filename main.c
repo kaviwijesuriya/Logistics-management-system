2c0b59e8ca62acc404745717e48df718aa366256
kaviwijesuriya <as20240493@sci.sjp.ac.lk>
2025-10-23 21:15:10 +0530

    implement menu for system

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CITIES 30
+
+
+int main() {
+
+        printf("\n===== Logistics Management System =====\n");
+        printf("1. Add City\n");
+        printf("2. Rename City\n");
+        printf("3. Remove City\n");
+        printf("4. Input Distance\n");
+        printf("5. Display Distance\n");
+        printf("6. Handle Delivery\n");
+        printf("7. Show Reports\n");
+        printf("8. Save and Exit\n");
+
+}
