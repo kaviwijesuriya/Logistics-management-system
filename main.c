1ff0a54b6bdf6cbabd3e2aef8082cc8db5445141
kaviwijesuriya <as20240493@sci.sjp.ac.lk>
2025-10-24 06:34:59 +0530

    implemented removeCity function

--- main.c
+++ main.c
@@ -6,10 +6,12 @@
 #define MAX_CITIES 30
 
 char cityName[MAX_CITIES][50];
+int distanceMatrix[MAX_CITIES][MAX_CITIES];
 int numOfCities = 0;
 
 void addCity();
 void renameCity();
+void removeCity();
 
 int main() {
     int choice;
@@ -31,9 +33,9 @@ int main() {
         switch (choice) {
             case 1: addCity(); break;
             case 2: renameCity(); break;
-
+            case 3: removeCity(); break;
         }
-    } while (choice != 2);
+    } while (choice != 3);
 
     return 0;
 }
@@ -72,3 +74,27 @@ void renameCity() {
     scanf("%s", cityName[idx]);
     printf("City renamed successfully!\n");
 }
+
+void removeCity() {
+    int idx;
+    if (numOfCities == 0) {
+        printf("No cities to remove.\n");
+        return;
+    }
+    printf("Enter city index (0 to %d): ", numOfCities - 1);
+    scanf("%d", &idx);
+    if (idx < 0 || idx >= numOfCities) {
+        printf("Invalid index.\n");
+        return;
+    }
+    for (int i = idx; i < numOfCities - 1; i++) {
+        strcpy(cityName[i], cityName[i + 1]);
+        for (int j = 0; j < numOfCities; j++) {
+            distanceMatrix[i][j] = distanceMatrix[i + 1][j];
+            distanceMatrix[j][i] = distanceMatrix[j][i + 1];
+        }
+    }
+    numOfCities--;
+    printf("City removed successfully!\n");
+}
+
